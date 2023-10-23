df70dbcbaee2c76c6b3fc12c8f6319e50df4b2dc
vbeaumel <valentin.beaumelle@etud.univ-jfc.fr>
2023-10-23 16:42:50 +0200

    Ajout de la classe ServicePrevu

--- src/main/java/champollion/ServicePrevu.java
+++ src/main/java/champollion/ServicePrevu.java
@@ -1,6 +1,25 @@
 package champollion;
 
 public class ServicePrevu {
-	// TODO : implémenter cette classe
+	private int volumeCM;
+    private int volumeTD;
+    private int volumeTP;
 
+    public ServicePrevu(int volumeCM, int volumeTD, int volumeTP) {
+        this.volumeCM = volumeCM;
+        this.volumeTD = volumeTD;
+        this.volumeTP = volumeTP;
+    }
+
+    public int getVolumeCM() {
+        return volumeCM;
+    }
+
+    public int getVolumeTD() {
+        return volumeTD;
+    }
+
+    public int getVolumeTP() {
+        return volumeTP;
+    }
 }
