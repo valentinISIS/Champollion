713b582a6ccb6661ac9c2d0304890997dd9a212c
vbeaumel <valentin.beaumelle@etud.univ-jfc.fr>
2023-10-23 17:38:37 +0200

    Ajout des classes et attributs manquants

--- src/main/java/champollion/UE.java
+++ src/main/java/champollion/UE.java
@@ -3,13 +3,23 @@ package champollion;
 public class UE {
     private final String myIntitule;
 
-    public UE(String intitule) {
-        myIntitule = intitule;
+    private int heureCM;
+    private int heureTD;
+    private int heureTP;
+
+    public UE(String myIntitule, int heureCM, int heureTD, int heureTP) {
+        this.myIntitule = myIntitule;
+        this.heureCM = heureCM;
+        this.heureTD = heureTD;
+        this.heureTP = heureTP;
     }
 
     public String getIntitule() {
         return myIntitule;
     }
 
+    public boolean ajoutValide(int heureCM, int heureTD, int heureTP){
+        return false;
+    }
     
 }
